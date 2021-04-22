a6e400bf5a677e3b6e8c7adb8c9af475327712f9
Half_Work <1325554393@qq.com>
2021-04-22 17:22:31 +0800

    tt

--- Boss.cpp
+++ Boss.cpp
@@ -1,20 +1,20 @@
-#include "Manager.h"
+#include "Boss.h"
 
 
-Manager::Manager(int id, string name, int dId)
+Boss::Boss(int id, string name, int dId)
 {
 	this->m_id = id;
 	m_Name = name;
 	m_Deptid = dId;
 }
 
-void Manager::showInfo()
+void Boss::showInfo()
 {
 	cout << "职工编号：  " << m_id << "\t姓名： " << m_Name << "\t岗位：  " << getDeptName()
 		<< "\t岗位职责： 完成老板给的任务" << endl;
 }
 
-string Manager::getDeptName()
+string Boss::getDeptName()
 {
 	return "经理";
 }
